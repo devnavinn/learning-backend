17b8187fd95faf8cad3df928220752b07aa0781f
neo062 <navinkumar241999@gmail.com>
2023-11-03 11:22:12 +0530

    Script to add data to db

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,52 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const fs = require('fs')
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log('DB connection successful!');
+})
+
+
+//Read json file
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+
+//Import data into db
+const importData = async () => {
+    try {
+        await Tour.create(tours);
+        console.log('Data successfully loaded!');
+
+    } catch (err) {
+        console.log(err);
+    }
+    process.exit();
+}
+
+//delete all data from collection
+const deleteData = async () => {
+    try {
+        await Tour.deleteMany();
+        console.log('Data successfully deleted!');
+
+    } catch (err) {
+        console.log(err);
+    }
+    process.exit();
+}
+if (process.argv[2] === '--import') {
+    console.log('add', process.argv[2]);
+    importData();
+}
+if (process.argv[2] === '--delete') {
+    console.log('delete', process.argv[2])
+    deleteData();
+}
+console.log(process.argv)
