0b7f6b71a26ed917d95ac3b4b337f2d63f7deb01
neo062 <navinkumar241999@gmail.com>
2024-01-12 17:40:02 +0530

    get single doument factory function

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -55,3 +55,17 @@ exports.CreateOne = Model => catchAsync(async (req, res, next) => {
 
 });
 
+exports.getOne = (Model, populateOptions) => catchAsync(async (req, res, next) => {
+    const query = Model.findById(req.params.id)
+    if (populateOptions) query.populate(populateOptions)
+    const doc = await query;
+    if (!doc) {
+        return next(new AppError('No document found with that ID', 404))
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            doc
+        }
+    })
+})
