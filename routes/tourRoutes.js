ab01d6689a20f3ae43237cd5e9a2c91bea76a5a4
neo062 <navinkumar241999@gmail.com>
2024-01-24 16:15:56 +0530

    authorization here

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -11,13 +11,21 @@ router.use('/:tourId/reviews', reviewRouter)
 
 router.route('/tour-stats').get(tourController.getTourStats)
 
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan)
+router.route('/monthly-plan/:year').get(authController.protect,
+    authController.restrictTo('admin', 'lead-guide', 'guide'),
+    tourController.getMonthlyPlan)
 
 router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getTours)
 
-router.route('/').get(authController.protect, tourController.getTours).post(tourController.createTour)
+router.route('/')
+    .get(tourController.getTours)
+    .post(authController.protect,
+        authController.restrictTo('admin', 'lead-guide'),
+        tourController.createTour)
 
-router.route('/:id').get(authController.protect, tourController.getTour).patch(authController.protect, tourController.updateTour)
+router.route('/:id')
+    .get(authController.protect, tourController.getTour)
+    .patch(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.updateTour)
     .delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour)
 
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,19 +7,22 @@ const authController = require('./../controllers/authController')
 
 router.post('/signup', authController.signup)
 router.post('/login', authController.login)
-
-
 router.patch('/verifyEmail/:token', authController.verifyEmail)
 router.post('/resendVerifyEmail', authController.resendVerifyEmail)
-
 router.post('/forgotPassword', authController.forgotPassword)
 router.patch('/resetPassword/:token', authController.resetPassword)
-router.patch('/updateMyPassword', authController.protect, authController.updatePassword)
 
-router.get('/me', authController.protect, userController.getMe, userController.getUser)
-router.patch('/updateMe', authController.protect, userController.updateMe)
-router.delete('/deleteMe', authController.protect, userController.deleteMe)
+// Protect all route after this
+router.use(authController.protect);
 
+router.patch('/updateMyPassword', authController.updatePassword)
+
+router.get('/me', userController.getMe, userController.getUser)
+router.patch('/updateMe', userController.updateMe)
+router.delete('/deleteMe', userController.deleteMe)
+
+// Operation allowed to admin only
+router.use(authController.restrictTo('admin'))
 router
     .route('/')
     .get(userController.getAllUsers)
