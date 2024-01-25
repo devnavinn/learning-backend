694e3bc289ebaece04af258d3e9332473e470887
neo062 <navinkumar241999@gmail.com>
2024-01-25 18:30:22 +0530

    static method and some new concepts

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -2,6 +2,8 @@
 
 const mongoose = require('mongoose')
 
+const Tour = require('./tourModel')
+
 const reviewSchema = new mongoose.Schema({
     review: {
         type: String,
@@ -50,6 +52,35 @@ reviewSchema.pre(/^find/, function (next) {
     })
 
     next()
+})
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+    // this refer to modal 
+    const stats = await this.aggregate([
+        {
+            $match: {
+                tour: tourId
+            },
+        },
+        {
+            $group: {
+                _id: '$tour',
+                nRating: { $sum: 1 },
+                avgRating: { $avg: '$rating' }
+            }
+        }
+
+    ])
+    await Tour.findByIdAndUpdate(tourId, {
+        ratingAverage: stats[0].nRating,
+        ratingQuantity: stats[0].avgRating
+    })
+}
+
+reviewSchema.post('save', function () {
+    //this points to current doucuments
+    this.constructor.calcAverageRatings(this.tour)
+
 })
 const Review = mongoose.model('Review', reviewSchema)
 
